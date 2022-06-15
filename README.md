86abf248897cf143f5cc17f3c05a343708154dcb
Calvin Cheng <linchuan.cheng@gmail.com>
2022-06-15 23:57:54 +0800

    golang training session with cobra and echo

---
+++ README.md
@@ -0,0 +1,13 @@
+# Get started with golang
+
+## cobra cli
+
+## echo framework
+
+## Run it
+
+```bash
+go build -o app
+./app server
+open http://localhost:1323
+```
---
+++ cmd/server.go
@@ -0,0 +1,45 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/spf13/cobra"
+)
+
+// serverCmd represents the server command
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		e := echo.New()
+		e.GET("/", func(c echo.Context) error {
+			return c.String(http.StatusOK, "Hello, World!")
+		})
+		e.Logger.Fatal(e.Start(":1323"))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serverCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// serverCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// serverCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
