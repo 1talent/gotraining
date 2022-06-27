f9abad7e0c5ec11991b82dd02120e683ba2d6aca
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2022-06-27 16:47:59 +0700

    Implement error handling + generic middlewares

---
+++ internal/api/router/error_handler.go
@@ -0,0 +1,126 @@
+package router
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/1talent/gotraining/internal/api/httperrors"
+	"github.com/1talent/gotraining/internal/types"
+	"github.com/go-openapi/swag"
+	"github.com/labstack/echo/v4"
+)
+
+var (
+	DefaultHttpErrorHandlerConfig = HTTPErrorHandlerConfig{
+		HideInternalServerErrorDetails: true,
+	}
+)
+
+type HTTPErrorHandlerConfig struct {
+	HideInternalServerErrorDetails bool
+}
+
+func HTTPErrorHandlerWithConfig(HideInternalServerErrorDetails bool) echo.HTTPErrorHandler {
+	return func(err error, c echo.Context) {
+		var code int64
+		var he error
+
+		var httpError *httperrors.HTTPError
+		var httpValidationError *httperrors.HTTPValidationError
+		var echoHTTPError *echo.HTTPError
+
+		switch {
+		case errors.As(err, &httpError):
+			code = *httpError.Code
+			he = httpError
+			if code == http.StatusInternalServerError && HideInternalServerErrorDetails {
+				if httpError.Internal == nil {
+					httpError.Internal = fmt.Errorf("Internal Error: %w", httpError)
+				}
+
+				httpError.Title = swag.String(http.StatusText(http.StatusInternalServerError))
+			}
+		case errors.As(err, &httpValidationError):
+			code = *httpValidationError.Code
+			he = httpValidationError
+
+			if code == http.StatusInternalServerError && HideInternalServerErrorDetails {
+				if httpValidationError.Internal == nil {
+					httpValidationError.Internal = fmt.Errorf("Internal Error: %w", httpValidationError)
+				}
+
+				httpValidationError.Title = swag.String(http.StatusText(http.StatusInternalServerError))
+			}
+		case errors.As(err, &echoHTTPError):
+			code = int64(echoHTTPError.Code)
+
+			if code == http.StatusInternalServerError && HideInternalServerErrorDetails {
+				if echoHTTPError.Internal == nil {
+					echoHTTPError.Internal = fmt.Errorf("Internal Error: %w", echoHTTPError)
+				}
+
+				he = &httperrors.HTTPError{
+					PublicHTTPError: types.PublicHTTPError{
+						Code:  swag.Int64(int64(echoHTTPError.Code)),
+						Title: swag.String(http.StatusText(http.StatusInternalServerError)),
+						Type:  swag.String(httperrors.HTTPErrorTypeGeneric),
+					},
+					Internal: echoHTTPError.Internal,
+				}
+			} else {
+				msg, ok := echoHTTPError.Message.(string)
+				if !ok {
+					if m, errr := json.Marshal(msg); err == nil {
+						msg = string(m)
+					} else {
+						msg = fmt.Sprintf("failed to marshal HTTP error message: %v", errr)
+					}
+				}
+
+				he = &httperrors.HTTPError{
+					PublicHTTPError: types.PublicHTTPError{
+						Code:  swag.Int64(int64(echoHTTPError.Code)),
+						Title: &msg,
+						Type:  swag.String(httperrors.HTTPErrorTypeGeneric),
+					},
+					Internal: echoHTTPError.Internal,
+				}
+			}
+		default:
+			code = http.StatusInternalServerError
+			if HideInternalServerErrorDetails {
+				he = &httperrors.HTTPError{
+					PublicHTTPError: types.PublicHTTPError{
+						Code:  swag.Int64(int64(http.StatusInternalServerError)),
+						Title: swag.String(http.StatusText(http.StatusInternalServerError)),
+						Type:  swag.String(httperrors.HTTPErrorTypeGeneric),
+					},
+
+					Internal: err,
+				}
+			} else {
+				he = &httperrors.HTTPError{
+					PublicHTTPError: types.PublicHTTPError{
+						Code:  swag.Int64(int64(http.StatusInternalServerError)),
+						Title: swag.String(err.Error()),
+						Type:  swag.String(httperrors.HTTPErrorTypeGeneric),
+					},
+				}
+			}
+		}
+
+		if !c.Response().Committed {
+			if c.Request().Method == http.MethodHead {
+				err = c.NoContent(int(code))
+			} else {
+				err = c.JSON(int(code), he)
+			}
+
+			if err != nil {
+				// util.LogFromEchoContext(c).Warn().Err(err).AnErr("http_err", err).Msg("Failed to handle HTTP error")
+			}
+		}
+	}
+}
--- internal/api/router/router.go
+++ internal/api/router/router.go
@@ -4,10 +4,59 @@ import (
 	"github.com/1talent/gotraining/internal/api"
 	"github.com/1talent/gotraining/internal/api/middleware"
 	"github.com/labstack/echo/v4"
+	echoMiddleware "github.com/labstack/echo/v4/middleware"
+	"github.com/rs/zerolog/log"
 )
 
 func Init(s *api.Server) {
 	s.Echo = echo.New()
+	s.Echo.HideBanner = true
+	s.Echo.HTTPErrorHandler = HTTPErrorHandlerWithConfig(true)
+
+	// General middleware
+	if s.Config.Echo.EnableTrailingSlashMiddleware {
+		s.Echo.Pre(echoMiddleware.RemoveTrailingSlash())
+	} else {
+		log.Warn().Msg("Disabling trailing slash middleware due to environment config")
+	}
+
+	if s.Config.Echo.EnableRecoverMiddleware {
+		s.Echo.Use(echoMiddleware.Recover())
+	} else {
+		log.Warn().Msg("Disabling recover middleware due to environment config")
+	}
+
+	if s.Config.Echo.EnableSecureMiddleware {
+		s.Echo.Use(echoMiddleware.SecureWithConfig(echoMiddleware.SecureConfig{
+			Skipper:               echoMiddleware.DefaultSecureConfig.Skipper,
+			XSSProtection:         s.Config.Echo.SecureMiddleware.XSSProtection,
+			ContentTypeNosniff:    s.Config.Echo.SecureMiddleware.ContentTypeNosniff,
+			XFrameOptions:         s.Config.Echo.SecureMiddleware.XFrameOptions,
+			HSTSMaxAge:            s.Config.Echo.SecureMiddleware.HSTSMaxAge,
+			HSTSExcludeSubdomains: s.Config.Echo.SecureMiddleware.HSTSExcludeSubdomains,
+			ContentSecurityPolicy: s.Config.Echo.SecureMiddleware.ContentSecurityPolicy,
+			CSPReportOnly:         s.Config.Echo.SecureMiddleware.CSPReportOnly,
+			HSTSPreloadEnabled:    s.Config.Echo.SecureMiddleware.HSTSPreloadEnabled,
+			ReferrerPolicy:        s.Config.Echo.SecureMiddleware.ReferrerPolicy,
+		}))
+	} else {
+		log.Warn().Msg("Disabling secure middleware due to environment config")
+	}
+
+	if s.Config.Echo.EnableRequestIDMiddleware {
+		s.Echo.Use(echoMiddleware.RequestID())
+	} else {
+		log.Warn().Msg("Disabling request ID middleware due to environment config")
+	}
+
+	if s.Config.Echo.EnableCORSMiddleware {
+		s.Echo.Use(echoMiddleware.CORS())
+	} else {
+		log.Warn().Msg("Disabling CORS middleware due to environment config")
+	}
+
+	// TODO: Implement specific middlewares: Logger, CacheControl, Profiler, Auth
+
 	s.Router = &api.Router{
 		Routes: nil, // will be populated by handlers.AttachAllRoutes(s)
 
